44e50eb24de2b242ecd066e11f838e114c7fe1a3
Michał Romaszkin <romaszkin13@o2.pl>
2019-11-11 14:55:12 +0100

    Add basic logic, add component to display results

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <div id="root">
-    <search-input />
+    <search-input v-on:searchQuery="fetchQuery" />
+    <search-results v-if="results.length > 0" :results="results" />
   </div>
 </template>
 
 <script>
 import SearchInput from "./components/SearchInput.vue";
+import SearchResults from "./components/SearchResults.vue";
+import axios from "axios";
 
 export default {
   name: "app",
+  data() {
+    return {
+      apiUrl: "https://api.themoviedb.org/3/search/movie",
+      apiKey: "8673a6b0eccdac7514050448c45f7d1a",
+      apiLang: "pl",
+      results: []
+    };
+  },
   components: {
-    SearchInput
+    SearchInput,
+    SearchResults
+  },
+  methods: {
+    fetchQuery: function(searchQuery) {
+      let request = `${this.apiUrl}?api_key=${this.apiKey}&language=${this.apiLang}&query=${searchQuery}&include_adult=false`;
+      this.getMovies(request);
+    },
+    getMovies: function(req) {
+      axios.get(req).then(res => {
+        console.log(res.data);
+        this.results = res.data.results;
+      });
+    }
   }
 };
 </script>
---
+++ src/components/SearchResults.vue
@@ -0,0 +1,31 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Tytuł</th>
+        <th>Popularność</th>
+        <th>Liczba głosów</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="result in results" v-bind:key="result.id">
+        <th>{{ result.id }}</th>
+        <th>{{ result.title }}</th>
+        <th>{{ result.popularity }}</th>
+        <th>{{ result.vote_count }}</th>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "SearchResults",
+  props: {
+    results: {
+      type: Array
+    }
+  }
+};
+</script>
